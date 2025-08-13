09a7e61873f77a47ac9cd7a8f440f21d933c04c8
naiem_sl <naiemulislam.dev@gmail.com>
2025-08-14 04:18:27 +0600

    git config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-  /* config options here */
+  basePath: '/flight-search-web-app',
+  trailingSlash: true,
 };
 
 export default nextConfig;
